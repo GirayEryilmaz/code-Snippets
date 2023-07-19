c6a67cff41eee5fe5fa20327fa1a3e784ee86c70
Giray <13612885+GirayEryilmaz@users.noreply.github.com>
2023-07-19 15:15:58 -0400

    Update seurat_get_column.r

--- R/seurat_get_column.r
+++ R/seurat_get_column.r
@@ -1,2 +1,5 @@
 pbmc[['column']] -> returns the column from the metadata as a DATAFRAME
 pbmc[['column', drop=TRUE]] or pbmc$column -> returns the column from the metadata as a VECTOR
+
+# multiple columns:
+pbmc[[c('col1', 'col2')]]
