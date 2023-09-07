ba3c0d21719ad254b29e8af526698298b35ebb11
Giray <13612885+GirayEryilmaz@users.noreply.github.com>
2023-09-07 16:54:40 -0400

    Update cell_proportions_plot.py

--- python/cell_proportions_plot.py
+++ python/cell_proportions_plot.py
@@ -1,10 +1,11 @@
 def cel_props_plot(adata, x_axis, to_stack):
-  """
-  Expects anndata (or mudata) and two column names from the obs dataframe
-  """
-    group = adata.obs[[x_axis, to_stack]].groupby([x_axis, to_stack])
-    df = group.value_counts()
-    percentages = (df/df.groupby(x_axis).sum())
-    percentages = percentages.unstack()
-
-    percentages.plot.bar(stacked=True, figsize=(10,10)).legend(bbox_to_anchor=(1.0, 0.5));
+	"""
+	Expects anndata (or mudata) and two column names from the obs dataframe
+	"""
+	group = adata.obs[[x_axis, to_stack]].groupby([x_axis, to_stack])
+	df = group.value_counts()
+	percentages = (df/df.groupby(x_axis).sum())
+	percentages = percentages.unstack()
+	
+	percentages.plot.bar(stacked=True, figsize=(10,10)).legend(bbox_to_anchor=(1.0, 0.5));
+	plt.grid(axis='x')
