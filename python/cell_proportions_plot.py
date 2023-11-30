15583eaa2911ace170e1968b22a836a5b0812b1c
Giray <13612885+GirayEryilmaz@users.noreply.github.com>
2023-11-30 11:36:22 -0500

    Update cell_proportions_plot.py
    improved longitudinal box plots with sns

--- python/cell_proportions_plot.py
+++ python/cell_proportions_plot.py
@@ -75,9 +75,11 @@ def longitudinal_cell_prop_pbmc_plot(adata, pbmc, cell_type_of_interest, facade 
     fig.subplots_adjust(wspace=wspace)
 
 def longitudinal_cell_prop_box_plot(adata, pbmc, cell_type_of_interest, facade = 'Vaccine', group_by = ['Visit', 'person'], 
-                                    annots_col = 'subset_annotations', include = 'all', wspace=0.3, figsize = (25, 3), rot = 90):
+                                    annots_col = 'subset_annotations', include = 'all', wspace=0.3, figsize = (25, 3), rot = 90, share_y = True):
     ncats = len(adata.obs[facade].cat.categories)
     fig, axs = plt.subplots(1, ncats, figsize = figsize)
+
+    perc_dfs = []
     
     for ax, vax in  zip(axs, adata.obs[facade].cat.categories):
         adata_vax = adata[adata.obs[facade] == vax]
@@ -87,15 +89,29 @@ def longitudinal_cell_prop_box_plot(adata, pbmc, cell_type_of_interest, facade =
         percentages_of_celltype = percentages.xs(cell_type_of_interest, level=2, drop_level=True).dropna(axis = 'rows')
         percentages_of_celltype = percentages_of_celltype.unstack()
         percentages_of_celltype = percentages_of_celltype.T
-
+        
         if include == 'all':
-            to_include = list(percentages_of_celltype.columns)
+            pass
         else:
             to_include = [i for i in include if i in percentages_of_celltype.columns]
+            percentages_of_celltype = percentages_of_celltype[to_include]
 
-        percentages_of_celltype[to_include].plot.box(ax = ax, rot = rot)
-            
+        perc_dfs.append(percentages_of_celltype)
+
+    for ax, vax, df in zip(axs, adata.obs[facade].cat.categories, perc_dfs):
+        ax = sns.boxplot(data=df, color = 'white', ax=ax)
+        ax = sns.swarmplot(data=df, color = 'gray', ax=ax)
+        ax = sns.lineplot(data=df.T, ax=ax)
+        ax.tick_params(axis='x', labelrotation=90)
+        ax.set_xlabel(None);
+        ax.legend(loc='center left', bbox_to_anchor=(1, 0.5));
         ax.set_title(f'{vax} - {cell_type_of_interest}')
         ax.grid(axis='x')
         ax.tick_params(axis=u'both', which=u'both',length=0)
+
+    if share_y:
+        y_max = max([df.values.max() for df in perc_dfs])
+        y_max = y_max*1.05
+        for ax in axs:
+            ax.set_ylim(top=y_max, bottom = 0)
     fig.subplots_adjust(wspace=wspace)
