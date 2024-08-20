c0cf338116da54c8f289ab634d3bcb7e5a321a43
Giray <13612885+GirayEryilmaz@users.noreply.github.com>
2024-08-20 15:13:05 -0400

    Create gseapy_examples.py

---
+++ python/gseapy_examples.py
@@ -0,0 +1,15 @@
+import gseapy as gp
+
+# Get avaliable libraries:
+gp.get_library_name()
+
+# Get the gene lists of a library (returns dict)
+gp.get_library('GO_Biological_Process_2023')
+
+# Gene set enrichment analysis with a gene list
+enr = gp.enrichr(gene_list=['ISG15', 'MX1', 'STAT2'],
+                 gene_sets=['MSigDB_Hallmark_2020','KEGG_2021_Human', 'GO_Biological_Process_2023', 'WikiPathway_2023_Human'],
+                 organism='human', # organism argment is ignored because user input a background
+                 outdir=None, # don't write to disk
+                )
+enr.results.sort_values(by = 'Combined Score', ascending=False)
