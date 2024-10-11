8333201da43e3946b7f70c790d4a9ec51e180d1b
Giray <13612885+GirayEryilmaz@users.noreply.github.com>
2024-10-11 11:42:10 -0400

    Added subclustering

--- python/scanpy/clustering_pipeline.py
+++ python/scanpy/clustering_pipeline.py
@@ -30,5 +30,27 @@ sc.tl.leiden(adata, resolution=0.3, key_added='l030')
 
 # ... Annotate Cells here using marker genes ... #
 
-# Save progress!
+# Save the progress!
 adata.write_h5ad('adata.h5ad')
+
+# ... Subclustering ... #
+
+subset = adata[adata.obs.cell_types == 'Cell Type'].copy()
+
+subset.X = subset.layers['counts'].copy()
+
+sc.pp.normalize_total(subset)
+
+# This gives a warning because of a bug : "adata.X seems to be already log-transformed."
+sc.pp.log1p(subset)
+
+sc.pp.highly_variable_genes(subset, batch_key='batch')
+
+sc.tl.pca(subset, svd_solver='arpack')
+
+# Skip if you are not going to use harmony for batch correction
+sc.external.pp.harmony_integrate(subset, 'batch')
+
+sc.pp.neighbors(subset, n_pcs=15)
+
+sc.tl.umap(subset)
